9673f61483f503e9a087aef33b225b9108a29b79
trong thuy <trongthuynl2004@gmail.com>
2024-10-04 17:15:19 +0700

    creat smartparking AppWinform

---
+++ stm32/stm32_smartparking/Winform/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnMode = new System.Windows.Forms.Button();
+            this.labelMode = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(239, 26);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(1);
+            this.label1.Size = new System.Drawing.Size(328, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Management Parking System";
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(614, 349);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(403, 349);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 49);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "History";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnMode
+            // 
+            this.btnMode.AutoSize = true;
+            this.btnMode.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMode.Location = new System.Drawing.Point(34, 95);
+            this.btnMode.Name = "btnMode";
+            this.btnMode.Size = new System.Drawing.Size(75, 30);
+            this.btnMode.TabIndex = 3;
+            this.btnMode.Text = "Mode";
+            this.btnMode.UseVisualStyleBackColor = true;
+            this.btnMode.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // labelMode
+            // 
+            this.labelMode.AutoSize = true;
+            this.labelMode.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.labelMode.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMode.Location = new System.Drawing.Point(161, 95);
+            this.labelMode.Name = "labelMode";
+            this.labelMode.Size = new System.Drawing.Size(95, 29);
+            this.labelMode.TabIndex = 4;
+            this.labelMode.Text = "Parking";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.button1;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelMode);
+            this.Controls.Add(this.btnMode);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.HelpButton = true;
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(1);
+            this.Text = "WinfromApp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnMode;
+        private System.Windows.Forms.Label labelMode;
+    }
+}
+
---
+++ stm32/stm32_smartparking/Winform/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool btnState = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!btnState)
+            {
+                button1.Text = "Disconnect";
+                btnState = true;
+            }
+            else 
+            {
+                button1.Text = "Connect";
+                btnState = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form f = new Form();
+            f.ShowDialog();
+            f.Text = "history";
+        }
+
+        bool labelMode_Status = false;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!labelMode_Status)
+            {
+                labelMode.Text = "Leaving";
+                labelMode_Status = true;
+            }
+            else 
+            {
+                labelMode.Text = "Parking";
+                labelMode_Status = false;
+            }
+        }
+    }
+}
